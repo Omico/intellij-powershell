7a35eea1d4c2009b2320bdd5d6b402fcd17eccac
Omico <me@omico.icu>
2023-02-12 14:41:42 -0800

    Download Plaster and PSScriptAnalyzer dynamically

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,8 +52,47 @@ dependencies {
     testImplementation("junit:junit:4.13.1")
 }
 
+val powershellModules = listOf("Plaster", "PSScriptAnalyzer")
+
+tasks {
+    powershellModules.forEach { module ->
+        register("download$module") {
+            group = "powershell"
+            doLast {
+                val dir = file(module)
+                if (dir.exists()) dir.deleteRecursively()
+                exec {
+                    commandLine("pwsh", "-Command", "Save-Module", module, ".")
+                }
+            }
+        }
+        register("findLatest$module") {
+            mustRunAfter("download$module")
+            group = "powershell"
+            val dirs = file(module).walk().maxDepth(1)
+            if (dirs.count() > 0) outputs.dirs(dirs.last())
+            doLast {
+                require(file(module).exists()) { "You should run \"gradle download$module\" first." }
+            }
+        }
+    }
+}
+
+val downloadMissingPowerShellModules by tasks.registering {
+    powershellModules
+        .filterNot { module -> file(module).exists() }
+        .map { module -> "download$module" }
+        .let { list -> dependsOn(list) }
+}
+
 tasks.prepareSandbox {
+    dependsOn(downloadMissingPowerShellModules)
     from("${project.rootDir}/language_host/current") {
         into("${intellij.pluginName.get()}/lib/")
+    }
+    powershellModules.forEach { module ->
+        from(tasks["findLatest$module"].outputs) {
+            into("${intellij.pluginName.get()}/lib/LanguageHost/modules/$module")
+        }
     }
 }
