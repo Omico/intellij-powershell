9396b468461389d8b9c27025be987acfe45b3a01
Omico <me@omico.icu>
2023-03-27 13:55:03 -0700

    Update IntelliJ plugin to 1.13.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
 }
 
 plugins {
-    id("org.jetbrains.intellij") version "1.13.1"
+    id("org.jetbrains.intellij") version "1.13.2"
     id("org.jetbrains.kotlin.jvm") version "1.8.10"
     id("com.diffplug.spotless") version "6.16.0"
     id("me.omico.age.spotless") version "1.0.0-SNAPSHOT"
